b1b32efd9d248f78e8c508cb2cdfc27588f968c3
veritascoding <jarrettcoding@gmail.com>
2021-12-16 20:05:08 -0600

    correct error in meal api url

--- js/script.js
+++ js/script.js
@@ -3,26 +3,27 @@ var coolMeals = [];
 var warmMeals = [];
 var hotMeals = [];
 
-var chosenMeal = ""
+var chosenMeal = "52772"
 
 
-var mealApiUrl = "www.themealdb.com/api/json/v1/1/lookup.php?i=" + chosenMeal;
+var mealApiUrl = "https://themealdb.com/api/json/v1/1/lookup.php?i=" + chosenMeal;
 
 var chosenCityTemp = ""
 
 var getMealId = function() {
 
     if (chosenCityTemp <= 30) {
-        chosenMeal = coldMeals[Math.floor(Math.random() * coldMeals.length)];
+        chosenMeal = coldMeals[Math.floor(Math.random() * (coldMeals.length-1))];
     } else if (chosenCityTemp > 30 && chosenCityTemp <= 60) {
-        chosenMeal = coolMeals[Math.floor(Math.random() * coolMeals.length)];
+        chosenMeal = coolMeals[Math.floor(Math.random() * (coolMeals.length-1))];
     } else if (chosenCityTemp > 60 && chosenCityTemp <= 80) {
-        chosenMeal = warmMeals[Math.floor(Math.random() * warmMeals.length)];
+        chosenMeal = warmMeals[Math.floor(Math.random() * (warmMeals.length-1))];
     } else if (chosenCityTemp > 80) {
-        chosenMeal = warmMeals[Math.floor(Math.random() * hotMeals.length)];
+        chosenMeal = warmMeals[Math.floor(Math.random() * (hotMeals.length-1))];
     } else {
         console.log("error on meal choice")
     };
+    getMeal();
 };
 
 
