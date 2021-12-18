2ad5a782f536f8041c68a88dea655075df9b4ef3
veritascoding <jarrettcoding@gmail.com>
2021-12-17 19:27:31 -0600

    define mealApiUrl in local scope and add comments

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
+// Array of meal IDs for MealDB API
 var coldMeals = [53057, 52803, 52942, 52903, 52788];
 var coolMeals = [53043, 52922, 52851, 52989, 52934];
 var warmMeals = [52944, 53038, 53051, 53062, 52871];
 var hotMeals = [52891, 52957, 52889, 53063, 52939];
 
-var chosenMeal = "52772"
+// selected meal ID for MealDB API
+var chosenMeal = ""
 
+// static variable MealDB API URL
+var mealApiUrl = "";
 
-var mealApiUrl = "https://themealdb.com/api/json/v1/1/lookup.php?i=" + chosenMeal;
+// Temperature in city input by user
+var chosenCityTemp = 30;
 
-var chosenCityTemp = ""
-
+// function to select random meal ID and run fetch to MealDB API
 var getMealId = function() {
-
+    
     if (chosenCityTemp <= 30) {
         chosenMeal = coldMeals[Math.floor(Math.random() * (coldMeals.length-1))];
     } else if (chosenCityTemp > 30 && chosenCityTemp <= 60) {
@@ -23,16 +27,18 @@ var getMealId = function() {
     } else {
         console.log("error on meal choice")
     };
+    mealApiUrl = "https://themealdb.com/api/json/v1/1/lookup.php?i=" + chosenMeal;
+    // console.log(chosenMeal);
     getMeal();
 };
 
-
+// function to fetch JSON data from MealDB API
 var getMeal = function() { 
     fetch(mealApiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    console.log(data)
+                    // console.log(data)
                 });
             } else {
                 console.log("error in JSON request")
