c79912c3f2907601e0f9fa33604ffa1df2893131
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2021-12-19 11:41:41 -0500

    added arrays for chosen drinks and random choice function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
+// Array of drink IDs
+var coldDrinks = [11634, 17267, 178358, 13936];
+var coolDrinks = [14282, 16987, 17239, 17255];
+var warmDrinks = [15933, 11326, 178334, 12560];
+var hotDrinks = [11320, 178352, 16485, 17212];
+
+// Selected drink ID
+var selectedDrink = "";
 
 
-function getCocktailInfo (){
- fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+// Static URL variable cocktail DB
+var cocktailApiUrl = "";
+
+
+// Temperature basis input
+var chosenCityTemp = 30;
+
+
+
+// Function to select drink based on temp
+var getDrinkId = function () {
+
+    if (chosenCityTemp <= 30) {
+        selectedDrink = coldDrinks[Math.floor(Math.random() * (coldDrinks.length-1))];
+    } else if (chosenCityTemp > 30 && chosenCityTemp <= 60) {
+        selectedDrink = coolDrinks[Math.floor(Math.random() * (coolDrinks.length-1))];
+    } else if (chosenCityTemp > 60 && chosenCityTemp <= 80) {
+        selectedDrink = warmDrinks[Math.floor(Math.random() * (warmDrinks.length-1))];
+    } else if (chosenCityTemp > 80) {
+        selectedDrink = hotDrinks[Math.floor(Math.random() * (hotDrinks.length-1))];
+    } else {
+        console.log("error");
+    };
+    cocktailApiUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + selectedDrink;
+    getDrink();
+}
+
+
+var getDrink = function () {
+ fetch(cocktailApiUrl)
     .then(
         function(response) {
             if (response.status !== 200) {
@@ -10,7 +46,6 @@ function getCocktailInfo (){
             }
 
             response.json().then(function(data) {
-                //console.log(data);
                 displayCocktailInfo(data);
             });
         }
@@ -18,12 +53,6 @@ function getCocktailInfo (){
     .catch(function(err) {
         console.log('Fetch Error', err);
     });
-}
-
-getCocktailInfo();
-
-function displayCocktailInfo(cocktail) {
-    console.log(cocktail);
-}
+};
 
 
